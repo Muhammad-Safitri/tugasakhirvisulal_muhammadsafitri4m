4e3a630fb96b395583c5344b7e85754f7ded4443
Muhammad-Safitri <muhammadsafitri014@gmail.com>
2023-07-09 01:18:42 +0800

    komit pertama

---
+++ TugasAkhir.dpr
@@ -0,0 +1,25 @@
+program TugasAkhir;
+
+uses
+  Forms,
+  frLogin in 'frLogin.pas' {Login},
+  frSiswa in 'frSiswa.pas' {Siswa},
+  frKelas in 'frKelas.pas' {Kelas},
+  frOrtu in 'frOrtu.pas' {Ortu},
+  frPoin in 'frPoin.pas' {Poin},
+  frWaliKelas in 'frWaliKelas.pas' {WaliKelas},
+  frHubungan in 'frHubungan.pas' {Hubungan};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TWaliKelas, WaliKelas);
+  Application.CreateForm(TSiswa, Siswa);
+  Application.CreateForm(TOrtu, Ortu);
+  Application.CreateForm(TKelas, Kelas);
+  Application.CreateForm(TPoin, Poin);
+  Application.CreateForm(TLogin, Login);
+  Application.CreateForm(THubungan, Hubungan);
+  Application.Run;
+end.
